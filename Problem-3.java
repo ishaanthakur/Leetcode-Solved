3e1f03aefd2c4e446d85e842c259ba06dc7016a0
Ishaan Thakur <ishaan.thakur57@gmail.com>
2020-03-28 13:37:04 -0400

    Third Question

---
+++ Problem-3.java
@@ -0,0 +1,21 @@
+/** Given a string, find the length of the longest substring without repeating characters. */
+import java.util.*;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
