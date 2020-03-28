44a2847456cdc8c7c687d1f2dbfd37b88be24f84
Ishaan Thakur <ishaan.thakur57@gmail.com>
2020-03-28 13:30:09 -0400

    First Question

---
+++ Problem-1.java
@@ -0,0 +1,27 @@
+/**Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice. */
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i< nums.length; i++){
+            map.put(nums[i], i);
+            
+        }
+        for(int i=0; i< nums.length; i++){
+            int hello = target - nums[i];
+            if(map.containsKey(hello) && map.get(hello) !=i){
+                return new int[]{map.get(hello),i };
+            }
+        } 
+            throw new IllegalArgumentException("hola");
+            
+    
+    
+    
+    
+    }
+}
